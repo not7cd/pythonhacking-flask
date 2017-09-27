861d999f1acdf9be4aecaf097fb455870d96192b
Norbert Szulc <norbert@not7cd.net>
2017-09-27 20:16:57 +0200

    Add view

--- src/views.py
+++ src/views.py
@@ -10,7 +10,7 @@ from flask_login import login_required, login_user,\
 from src import app, db, login_manager
 from src.forms import TaskForm, LoginForm, SignInForm
 from src.models import User, Task
-
+from src.ical_adapter import import_ical_to_tasks
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -29,7 +29,7 @@ def sign_up():
     form = SignInForm()
     if form.validate_on_submit():
         user = User(
-            name=form.name.data,
+            name=form.user.data,
             password=form.password.data,
             email=form.email.data,
         )
@@ -45,9 +45,9 @@ def login():
     """Login view"""
     form = LoginForm()
     if form.validate_on_submit():
-        name = form.name.data
+        username = form.username.data
         password = form.password.data
-        user = User.get_by_name(name)
+        user = User.get_by_name(username)
         if user is not None and user.check_password(password):
             should_stay_logged = form.remember_me.data
             login_user(user, should_stay_logged)
@@ -67,7 +67,7 @@ def logout():
 
 @app.route('/task', methods=['GET', 'POST'])
 @login_required
-def add_task():
+def add_car():
     """View for adding new car"""
     form = TaskForm()
     if form.validate_on_submit():
@@ -78,7 +78,24 @@ def add_task():
         db.session.commit()
         flash('Zapisano zadanie: {}'.format(description))
         return redirect(url_for('index'))
-    return render_template('add_task.html', form=form)
+    return render_template('add_car.html', form=form)
+
+@app.route('/import', methods=['GET', 'POST'])
+@login_required
+def import_ical():
+    """View for adding new car"""
+    form = ImportForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        tasks = import_ical_to_model()
+        task_num = len(tasks)
+        for task in tasks:
+            db.session.add(task)
+        db.session.commit()
+        flash('Zaimportowano {} elementów'.format(task_num))
+        return redirect(url_for('index'))
+    # TODO: add template
+    return render_template('add_car.html', form=form)
 
 
 @app.route('/some_json')
